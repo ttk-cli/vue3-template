186917b52ddedd4802fe171afe0f7e066ebafc5f
allen <272970666@qq.com>
2022-07-30 18:27:26 +0800

    chore: update unocss

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,13 @@
-import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
+import { defineConfig, Preset, presetAttributify, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
-  presets: [presetAttributify(), presetUno(), presetIcons()],
+  presets: [
+    presetAttributify(),
+    presetUno(),
+    presetIcons(),
+    presetRemToPx({
+      baseFontSize: 4,
+    }) as Preset,
+  ],
 })
